f3499a942eed905dd7ed46c91c55dbb8cc82585a
MorganDourdoigne <dourdoigne.morgan@gmail.com>
2023-09-13 16:15:58 +0200

    style.css

--- projet8/src/index.js
+++ projet8/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "./sass/Style.css";
+import "./sass/style.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
